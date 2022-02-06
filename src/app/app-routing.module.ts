2448944d54680e8ee724813595bda57657ebe519
nodurian <kuoadrian@gmail.com>
2022-02-05 19:47:38 -0700

    Remove / redirect from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { WorkComponent } from './work/work.component';
 // import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'about', component: AboutComponent },
   { path: 'work', component: WorkComponent },
